1d6bd076a687b2a23a2f5d6cf24bd4ee9e6cb4a1
PS- <2008qwe123@gmail.com>
2020-11-06 00:51:41 +0900

    push codeforce #681 -A

---
+++ Codeforce/Codeforce #681/A_Kids Seating.cpp
@@ -0,0 +1,47 @@
+/*
+* Problem A : https://codeforces.com/contest/1443/problem/A
+* 
+*/
+
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <deque>
+#include <tuple>
+#include <list>
+#include <set>
+#include <cmath>
+#include <stack>
+#include <map>
+//#include<bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+
+#define For(i, n) for(int i = 0; i < n; i++)
+#define IOS  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl  "\n";
+typedef  long long ll;
+
+
+void solved() {
+    IOS;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << 4 * n - i * 2 << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    IOS;
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solved();
+    }
+
+}
