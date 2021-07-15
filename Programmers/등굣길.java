bf92e1a70452fd95d9871061ff39957cfe3a96a4
yhsim98 <2008qwe123@gmail.com>
2021-07-15 16:53:05 +0900

    2021.07.15

---
+++ Programmers/등굣길.java
@@ -0,0 +1,36 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42898
+//어려운 문제는 아닌데 완탐과 메모이제이션으로 접근을 해서ㅠ 
+//c++이면 더 쉽게 풀었을거라는 생각이 드네요 
+//최근에 자바만 했는데 아직도 자바보다는 c++이 더 익숙합니다
+
+import java.util.*;
+
+public class Solution {
+  public int solution(int m, int n, int[][] puddles) {
+    int[][] street = new int[n][m];
+
+    // 웅덩이는 -1
+    for (int[] puddle : puddles)
+      street[puddle[1] - 1][puddle[0] - 1] = -1;
+
+    street[0][0] = 1;
+
+    for (int i = 0; i < n; i++) { // 시작점은 1로 저장
+      for (int j = 0; j < m; j++) {
+
+        if(street[i][j] == -1) { // 웅덩이면 0으로 바꾸고 continue
+          street[i][j] = 0;
+          continue;
+        }
+
+        if(i != 0)
+          street[i][j] += street[i - 1][j] % 1000000007; // 숫자가 이 값을 초과할 수 있기 때문에 계산 과정에서 나머지 구하기
+
+        if(j != 0)
+          street[i][j] += street[i][j - 1] % 1000000007; // 왼쪽
+      }
+    }
+
+    return street[n - 1][m - 1] % 1000000007;
+  }
+}
